ec254ad341bf2e38ebde94f8f6f850f5da539ed5
Philipp Hallgarten <ugeto@student.kit.edu>
2021-12-27 13:44:17 +0100

    added make_admin cli command

--- app/ftc/db.py
+++ app/ftc/db.py
@@ -27,10 +27,15 @@ def init_db():
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
 
-def load_players():
+def load_players(year):
     db = get_db()
-    with current_app.open_resource('players21.sql') as f:
+    with current_app.open_resource(f"players{year}.sql") as f:
         db.executescript(f.read().decode('utf8'))
+
+def make_admin(id):
+    db = get_db()
+    db.execute("UPDATE user SET isAdmin=1 WHERE ID=?",(id,))
+    db.commit()
 
 
 @click.command('init-db')
@@ -41,13 +46,26 @@ def init_db_command():
     click.echo('Initialized the database.')
 
 @click.command('load-players')
+@click.argument('year')
 @with_appcontext
-def load_players_command():
-    """Load the FIFA21 players."""
-    load_players()
-    click.echo('Players loaded.')
+def load_players_command(year):
+    """Load the FIFA players."""
+    try:
+        load_players(year)
+        click.echo(f'FIFA{year} Players loaded.')
+    except FileNotFoundError:
+        click.echo('Error: Players not available. Try another year, e.g. 21 or 22')
+
+@click.command('make-admin')
+@click.argument('id')
+@with_appcontext
+def make_admin_command(id):
+    """Make a user an admin."""
+    make_admin(id)
+    click.echo(f'User {id} is now an admin.')    
 
 def init_app(app):
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
     app.cli.add_command(load_players_command)
+    app.cli.add_command(make_admin_command)
