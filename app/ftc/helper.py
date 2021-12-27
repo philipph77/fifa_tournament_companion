fc732cb3544e15b7762981c02fd647ef4d4f1132
Philipp Hallgarten <ugeto@student.kit.edu>
2021-12-27 17:57:11 +0100

    improved table sorting for standings tables

--- app/ftc/helper.py
+++ app/ftc/helper.py
@@ -64,7 +64,7 @@ def calculateTables(db):
             'Points': total_points
             },
             ignore_index=True)
-        table.sort_values('Points', ascending=False, inplace=True)
+        table.sort_values(by=['Points', 'Goal_Difference', 'Goals_For', 'Wins'], ascending=False, inplace=True)
         home_table = home_table.append({
             'Team': team_name,
             'GamesPlayed': home_games_played,
@@ -77,7 +77,7 @@ def calculateTables(db):
             'Points': home_points
             },
             ignore_index=True)
-        home_table.sort_values('Points', ascending=False, inplace=True)
+        home_table.sort_values(by=['Points', 'Goal_Difference', 'Goals_For', 'Wins'], ascending=False, inplace=True)
         away_table = away_table.append({
             'Team': team_name,
             'GamesPlayed': away_games_played,
@@ -90,26 +90,32 @@ def calculateTables(db):
             'Points': away_points
             },
             ignore_index=True)
-        away_table.sort_values('Points', ascending=False, inplace=True)
+        away_table.sort_values(by=['Points', 'Goal_Difference', 'Goals_For', 'Wins'], ascending=False, inplace=True)
     
+    table.reset_index(drop=True, inplace=True)
     table.index +=1
+    home_table.reset_index(drop=True, inplace=True)
     home_table.index +=1
+    away_table.reset_index(drop=True, inplace=True)
     away_table.index +=1
 
     return table, home_table, away_table
 
 def calculateScorerStats(db):
-    scorer = db.execute('SELECT players.short_name, COUNT(*) AS `points` FROM goals LEFT JOIN players on players.ID = goals.Scorer_ID WHERE goals.was_Owngoal=0 GROUP BY Scorer_ID')
+    scorer = db.execute('SELECT players.short_name, COUNT(*) AS `points` FROM goals LEFT JOIN players on players.ID = goals.Scorer_ID WHERE goals.was_Owngoal=0 GROUP BY Scorer_ID ORDER BY points DESC LIMIT 10')
     scorer = pd.DataFrame(data=scorer.fetchall(), columns=['Player', 'Goals'])
     scorer.sort_values('Goals', ascending=False, inplace=True)
+    scorer.reset_index(drop=True, inplace=True)
     scorer.index +=1
-    assist =db.execute('SELECT players.short_name, COUNT(*) AS `points` FROM goals LEFT JOIN players on players.ID = goals.Wingman_ID WHERE goals.was_Owngoal=0 AND goals.Wingman_ID IS NOT NULL GROUP BY Wingman_ID')
+    assist =db.execute('SELECT players.short_name, COUNT(*) AS `points` FROM goals LEFT JOIN players on players.ID = goals.Wingman_ID WHERE goals.was_Owngoal=0 AND goals.Wingman_ID IS NOT NULL GROUP BY Wingman_ID ORDER BY points DESC LIMIT 10')
     assist = pd.DataFrame(data=assist.fetchall(), columns=['Player', 'Assists'])
     assist.sort_values('Assists', ascending=False, inplace=True)
+    assist.reset_index(drop=True, inplace=True)
     assist.index +=1
-    penaldo = db.execute('SELECT players.short_name, COUNT(*) AS `points` FROM goals LEFT JOIN players on players.ID = goals.Scorer_ID WHERE goals.was_Owngoal=0 AND goals.was_Penalty=1 GROUP BY Scorer_ID')        
+    penaldo = db.execute('SELECT players.short_name, COUNT(*) AS `points` FROM goals LEFT JOIN players on players.ID = goals.Scorer_ID WHERE goals.was_Owngoal=0 AND goals.was_Penalty=1 GROUP BY Scorer_ID ORDER BY points DESC LIMIT 10')        
     penaldo = pd.DataFrame(data=penaldo.fetchall(), columns=['Player', 'Penalties'])
     penaldo.sort_values('Penalties', ascending=False, inplace=True)
+    penaldo.reset_index(drop=True, inplace=True)
     penaldo.index +=1
     
     return scorer, assist, penaldo
@@ -117,12 +123,16 @@ def calculateScorerStats(db):
 def calculateCardStats(db):
     gamer_ids = db.execute('SELECT ID FROM gamer').fetchall()
     gamer_ids = [int(id[0]) for id in gamer_ids]
-    yellow_cards = db.execute('SELECT players.short_name, COUNT(*) AS `points` FROM cards LEFT JOIN players on players.ID = cards.ReceivingPlayer WHERE cards.wasYellowCard=1 GROUP BY ReceivingPlayer')
+    yellow_cards = db.execute('SELECT players.short_name, COUNT(*) AS `points` FROM cards LEFT JOIN players on players.ID = cards.ReceivingPlayer WHERE cards.wasYellowCard=1 GROUP BY ReceivingPlayer ORDER BY points DESC LIMIT 10')
     yellow_cards = pd.DataFrame(data=yellow_cards.fetchall(), columns=['Name', 'Yellow Cards'])
     yellow_cards.sort_values(yellow_cards.columns[1], ascending=False, inplace=True)
-    red_cards = db.execute('SELECT players.short_name, COUNT(*) AS `points` FROM cards LEFT JOIN players on players.ID = cards.ReceivingPlayer WHERE cards.wasYellowCard=0 GROUP BY ReceivingPlayer')
+    yellow_cards.reset_index(drop=True, inplace=True)
+    yellow_cards.index +=1
+    red_cards = db.execute('SELECT players.short_name, COUNT(*) AS `points` FROM cards LEFT JOIN players on players.ID = cards.ReceivingPlayer WHERE cards.wasYellowCard=0 GROUP BY ReceivingPlayer ORDER BY points DESC LIMIT 10')
     red_cards = pd.DataFrame(data=red_cards.fetchall(), columns=['Name', 'Red Cars'])
     red_cards.sort_values(red_cards.columns[1], ascending=False, inplace=True)
+    red_cards.reset_index(drop=True, inplace=True)
+    red_cards.index +=1
     
     return yellow_cards, red_cards
 
