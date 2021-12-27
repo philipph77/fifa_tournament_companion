936046a322233ec74caff7f72a7c1957b9fe6330
Philipp Hallgarten <ugeto@student.kit.edu>
2021-12-27 14:00:19 +0100

    bugfix

--- app/tests/data.sql
+++ app/tests/data.sql
@@ -5,9 +5,10 @@ VALUES
   'johndoe77',
   'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f'
   );
-INSERT INTO gamer (FirstName, LastName, TeamName)
+INSERT INTO gamer (UserID, FirstName, LastName, TeamName)
 VALUES
   (
+  1,
   'John',
   'Doe',
   'Myteam 1903 e.V.'
